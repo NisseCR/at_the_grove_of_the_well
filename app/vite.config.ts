6320faa6eac842771df60b92157955a24351bd37
NisseCR <nissehermsen@gmail.com>
2026-06-18 20:35:11 +0200

    expose port 8000 for local dev ws-relay

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,4 +3,12 @@ import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    proxy: {
+      "/api/control/ws": {
+        target: "http://localhost:8000",
+        ws: true,
+      },
+    },
+  },
 });
